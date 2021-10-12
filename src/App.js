e0032835f7b7f29df5d043638c94697effeef2e2
Frédéric Reis <frederic.reis@uni.lu>
2021-10-12 18:45:36 +0200

    Add scroll memory + active menu issue when go back

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
+import ScrollMemory from 'react-router-scroll-memory';
 import AppRoutes from './AppRoutes';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -6,6 +7,7 @@ import Header from './components/Header';
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollMemory />
       <Header />
       <main className="mt-5">
         <AppRoutes/>
